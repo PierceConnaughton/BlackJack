6be732d75682c7d0cb9d9a4ca351b30125e41b9f
S00185812 <S00185812@VLAB-KCOMPY2-2>
2020-01-25 18:14:52 +0000

    Add project files.

---
+++ BlackJack/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlackJack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+
+        int playerNum = 0;
+        int dealerNum = 0;
+
+        string dealerNumString;
+        string playerNumString;
+        
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtBxEnterName.Text != null)
+            {
+                MessageBox.Show("Not Null");
+
+                int firstCard = rnd.Next(1, 11);
+                int secondCard = rnd.Next(1, 12);
+                int dealerFirstCard = rnd.Next(1, 11);
+
+                playerNum = firstCard + secondCard;
+
+                playerNumString = playerNum.ToString();
+
+                txtBlPlayerTotal.Text = playerNumString;
+
+                
+
+                dealerNum = dealerFirstCard;
+
+                dealerNumString = dealerNum.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Enter Name First");
+            }
+        }
+    }
+}
