cf91a20ac28b742a82caba394b59c45417169caf
Ti43652 <47326833+Ti43652@users.noreply.github.com>
2020-02-28 08:15:35 +0000

    Add files via upload

---
+++ Project/BlackJack/SearchRecordsWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace BlackJack
+{
+    /// <summary>
+    /// Interaction logic for SearchRecordsWindow.xaml
+    /// </summary>
+    public partial class SearchRecordsWindow : Window
+    {
+        List<Player> Allplayers = new List<Player>();
+
+        public SearchRecordsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+        
+            //H:\Year Two\Semester 4\Programming\Project\PlayerRecords.txt"
+
+            //when the search window is loaded
+            FileStream fs = new FileStream(@"D:\College\Programming\BlackJack-master\Project\PlayerRecords.txt", FileMode.Open, FileAccess.Read);
+
+            StreamReader sr = new StreamReader(fs);
+
+            
+            string name = TxtBxSearchName.Text;
+            string[] lines = new string[100];
+            bool found = false;
+            string recordInfo = "Error";
+            string result = "Unknown";
+            string searchName = string.Format(" {0,-15} Wins", name);
+
+            string lineIn = sr.ReadLine();
+            string[] fieldArray1 = new string[5];
+
+            if (name == "")
+            {
+                MessageBox.Show("Please enter name first");
+                return;
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                lines[i] = lineIn;
+                lineIn = sr.ReadLine();
+            }
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                fieldArray1 = lines[i].Split(':');
+                string playerName = string.Format(fieldArray1[1]);
+                if (fieldArray1[1] == searchName)
+                {
+                    result = string.Format(lines[i].ToString());
+
+                    MessageBox.Show(result);
+                    found = true;
+                    break;
+                }
+                
+
+            }
+            if (found == false)
+            {
+                result = "Player not found";
+                MessageBox.Show(result);
+
+
+            }
+            found = false;
+
+
+            sr.Close();
+        }
+
+        private void SearchRecordsWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+
+
+        }
+    }
+}
